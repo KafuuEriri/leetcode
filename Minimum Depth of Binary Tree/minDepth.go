4301356347df938ebfc9aa45ef3238725124b5ae
KafuuEriri <471881465@qq.com>
2022-08-09 22:44:41 +0800

    二叉树最小深度

---
+++ Minimum Depth of Binary Tree/minDepth.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+var root *TreeNode = &TreeNode{
+	Val: 2,
+	Left: &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 4,
+		},
+		Right: &TreeNode{
+			Val: 5,
+		},
+	},
+	Right: &TreeNode{
+		Val: 3,
+		Right: &TreeNode{
+			Val: 4,
+		},
+	},
+}
+
+func main() {
+	fmt.Println(minDepth(root))
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := minDepth(root.Left)
+	right := minDepth(root.Right)
+	if left != 0 && right != 0 {
+		return min(left, right) + 1
+	} else {
+		return max(left, right) + 1
+	}
+}
+
+func min(x, y int) int {
+	if x >= y {
+		return y
+	}
+	return x
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
